18d0ec32e553d7cface893cc8b10d1973feebe9f
unknown <you@example.com>
2020-06-10 10:56:48 +0200

    added homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.scss';
+import directory from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+  <Directory />
+)
+
+export default HomePage;
